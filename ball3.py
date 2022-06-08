f039e09b112f4e517ea5d74772cbaf110ed0bb7a
Guido Pérez Moro <104775061+Guigrosso@users.noreply.github.com>
2022-06-08 10:53:49 -0300

    Update ball3.py
    Traducido al español

--- ball3.py
+++ ball3.py
@@ -2,65 +2,65 @@ import sys, pygame
 # Inicializamos pygame
 pygame.init()
 # Muestro una ventana de 800x600
-size = 800, 600
-screen = pygame.display.set_mode(size)
+tamanio = 800, 600
+pantalla = pygame.display.set_mode(tamanio)
 # Cambio el título de la ventana
-pygame.display.set_caption("Juego BALL")
+pygame.display.set_caption("Juego pelota")
 # Inicializamos variables
-width, height = size
-speed = [1, 1]
-white = 255, 255, 255
-black = 0, 0, 0
+ancho, altura = tamanio
+velocidad = [1, 1]
+blanco = 255, 255, 255
+negro = 0, 0, 0
 #fondo = pygame.image.load("fondo.png").convert()
 # Crea un objeto imagen pelota y obtengo su rectángulo
-ballPic="ball2.png"
-ball = pygame.image.load(ballPic)
-ballrect = ball.get_rect()
+pelotaPng="pelota.png"
+pelota = pygame.image.load(pelotaPng)
+pelotaRectangulo = pelota.get_rect()
 # Crea un objeto imagen bate y obtengo su rectángulo
 batePic="bate.png"
 bate = pygame.image.load(batePic)
 bate1 = pygame.image.load(batePic)
-baterect = bate.get_rect()
-baterect1 = bate1.get_rect()
+bateRectangulo = bate.get_rect()
+bateRectangulo1 = bate1.get_rect()
 # Pongo el bate en el centro de la pantalla
-ballrect.move_ip(size[0]/2,size[1]/2)
-baterect.move_ip(size[0]/5,size[1]/2)
-baterect1.move_ip(size[0]*4/5,size[1]/2)
+pelotaRectangulo.move_ip(tamanio[0]/2,tamanio[1]/2)
+bateRectangulo.move_ip(tamanio[0]/5,tamanio[1]/2)
+bateRectangulo1.move_ip(tamanio[0]*4/5,tamanio[1]/2)
 # Comenzamos el bucle del juego
-run=True
-while run:
+corriendo=True
+while corriendo:
     # Espero un tiempo (milisegundos) para que la pelota no vaya muy rápida
     pygame.time.delay(3)
-    # Capturamos los eventos que se han producido
-    for event in pygame.event.get():
-        #Si el evento es salir de la ventana, terminamos
-        if event.type == pygame.QUIT: run = False
+    # Capturamos los eventoos que se han producido
+    for evento in pygame.event.get():
+        #Si el eventoo es salir de la ventana, terminamos
+        if evento.type == pygame.QUIT: corriendo = False
     # Compruebo si se ha pulsado alguna tecla
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        baterect = baterect.move(0, -1)
-    if keys[pygame.K_s]:
-        baterect = baterect.move(0, 1)
+    teclas = pygame.key.get_pressed()
+    if teclas[pygame.K_w]:
+        bateRectangulo = bateRectangulo.move(0, -1)
+    if teclas[pygame.K_s]:
+        bateRectangulo = bateRectangulo.move(0, 1)
     # Compruebo si hay colisión
-    if baterect.colliderect(ballrect) or baterect1.colliderect(ballrect):
-        speed[0] = - speed[0]
-    if keys[pygame.K_UP]:
-        baterect1=baterect1.move(0, -1)
-    if keys[pygame.K_DOWN]:
-        baterect1=baterect1.move(0, 1)
+    if bateRectangulo.colliderect(pelotaRectangulo) or bateRectangulo1.colliderect(pelotaRectangulo):
+        velocidad[0] = - velocidad[0]
+    if teclas[pygame.K_UP]:
+        bateRectangulo1=bateRectangulo1.move(0, -1)
+    if teclas[pygame.K_DOWN]:
+        bateRectangulo1=bateRectangulo1.move(0, 1)
     # Muevo la pelota
-    ballrect = ballrect.move(speed)
+    pelotaRectangulo = pelotaRectangulo.move(velocidad)
     # Compruebo si la pelota llega a los límites de la ventana
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
+    if pelotaRectangulo.left < 0 or pelotaRectangulo.right > ancho:
+        velocidad[0] = -velocidad[0]
+    if pelotaRectangulo.top < 0 or pelotaRectangulo.bottom > altura:
+        velocidad[1] = -velocidad[1]
     # Pinto el fondo de blanco, dibujo la pelota y actualizo la pantalla
-    screen.fill(black)
-    #screen.blit(fondo, [0, 40])
-    screen.blit(ball, ballrect)
-    screen.blit(bate1, baterect1)
-    screen.blit(bate, baterect)
+    pantalla.fill(negro)
+    #pantalla.blit(fondo, [0, 40])
+    pantalla.blit(pelota, pelotaRectangulo)
+    pantalla.blit(bate1, bateRectangulo1)
+    pantalla.blit(bate, bateRectangulo)
 
     pygame.display.flip()
 # Salgo de pygame

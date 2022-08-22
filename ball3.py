eb44aa51c75566708960cecc48a80601a43ac551
Mauro Ortiz <104775731+OrtizMLM@users.noreply.github.com>
2022-08-22 13:37:51 -0300

    Bloques
    Añadimos bloques

--- ball3.py
+++ ball3.py
@@ -1,5 +1,6 @@
 import sys, pygame
 
+
 # Inicializamos pygame
 pygame.init()
 
@@ -16,7 +17,7 @@ pygame.display.set_caption("Juego pelota")
 
 # Inicializamos variables
 ancho, altura = tamanio
-velocidad = 3
+velocidad = 2
 velocidadPelota = [velocidad, velocidad]
 velocidadBate = [velocidad, velocidad]
 blanco = 255, 255, 255
@@ -31,12 +32,17 @@ pelotaRectangulo = pelota.get_rect()
 bate1 = pygame.image.load("bate.png")
 bate = pygame.image.load("bate.png")
 bateRectangulo = bate.get_rect()
-bate1Rectangulo = bate1.get_rect()
+bate1Rectangulo = bate1.get_rect() 
+bloques = pygame.image.load("bate.png")
+bloquefijo = bloques.get_rect()
+bloquefijo1 = bloques.get_rect()
 
 # Pongo el bate en el centro de la pantalla
 pelotaRectangulo.move_ip(tamanio[0]/2,tamanio[1]/2)
 bateRectangulo.move_ip(tamanio[0]/4,tamanio[1]/2)
 bate1Rectangulo.move_ip(tamanio[0]*3/4,tamanio[1]/2)
+bloquefijo.move_ip(tamanio[0]/8,tamanio[1]/4)
+bloquefijo1.move_ip(tamanio[0]/8,tamanio[1]/2)
 
 
 # Comenzamos el bucle del juego
@@ -61,6 +67,12 @@ while corriendo:
     # Compruebo si hay colisión
     if bateRectangulo.colliderect(pelotaRectangulo) or bate1Rectangulo.colliderect(pelotaRectangulo):
         velocidadPelota[0] = - velocidadPelota[0]
+    if bloquefijo.colliderect(pelotaRectangulo):
+        velocidadPelota[0] = - velocidadPelota[0]
+        bloquefijo.move_ip(tamanio[0],tamanio[0])
+    if bloquefijo1.colliderect(pelotaRectangulo):
+        velocidadPelota[0] = - velocidadPelota[0]
+        bloquefijo1.move_ip(tamanio[0],tamanio[0])
     if teclas[pygame.K_UP]:
         bateRectangulo=bateRectangulo.move(0, -velocidadBate[1])
     if teclas[pygame.K_DOWN]:
@@ -74,6 +86,18 @@ while corriendo:
         velocidadPelota[0] = -velocidadPelota[0]
     if pelotaRectangulo.top < 0 or pelotaRectangulo.bottom > altura:
         velocidadPelota[1] = -velocidadPelota[1]
+    
+    # Compruebo si los bates llegan a los límites de la ventana
+    if bate1Rectangulo.left < 0 or bate1Rectangulo.right > ancho:
+        velocidadBate[0] = -velocidadBate[0]
+    if bate1Rectangulo.top < 0 or bate1Rectangulo.bottom > altura:
+        velocidadBate[1] = -velocidadBate[1]
+        
+    if bateRectangulo.left < 0 or bateRectangulo.right > ancho:
+        velocidadBate[0] = -velocidadBate[0]
+    if bateRectangulo.top < 0 or bateRectangulo.bottom > altura:
+        velocidadBate[1] = -velocidadBate[1]
+            
         
     # Pinto el fondo de blanco, dibujo la pelota y actualizo la pantalla
     pantalla.fill(negro)
@@ -81,8 +105,8 @@ while corriendo:
     pantalla.blit(pelota, pelotaRectangulo)
     pantalla.blit(bate1, bate1Rectangulo)
     pantalla.blit(bate, bateRectangulo)
-    
-
+    pantalla.blit(bloques, bloquefijo)
+    pantalla.blit(bloques, bloquefijo1)
     pygame.display.flip()
     
 # Salgo de pygame
